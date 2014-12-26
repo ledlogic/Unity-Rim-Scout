ebc058c749d8fc780b8ed04f8a1918ce57b4c494
Jeff Conrad <ledlogic@gmail.com>
2014-12-26 14:24:06 -0600

    Add explosions.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject asteroidExplosionTemplate;
+	public GameObject playerExplosionTemplate;
+
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Boundary") {
 			return;
 		}
+
+		// create asteroid explosion
+		Instantiate (asteroidExplosionTemplate, transform.position, transform.rotation);
+
+		// also create player explosion
+		if (other.tag == "Player") {
+			Instantiate(playerExplosionTemplate, other.transform.position, other.transform.rotation);
+		}
+
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 	}
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlasmaBoltMover : MonoBehaviour {
+public class Mover : MonoBehaviour {
 
 	public float speed;
 
