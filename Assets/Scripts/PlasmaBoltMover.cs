9180ac5c340fbba7efb9e053ffddf9ead15b316a
Jeff Conrad <ledlogic@gmail.com>
2014-12-26 12:52:41 -0600

    Created plasma bolt

---
+++ Assets/Scripts/PlasmaBoltMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlasmaBoltMover : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 velocity = transform.forward * speed;
+		rigidbody.velocity = velocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
